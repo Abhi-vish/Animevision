acd09e03cf305c4127ffb2fbaa2272491753e64e
Abhi-vish <iamabhishekvishwakarma7@gmail.com>
2024-06-20 15:50:36 +0530

    Intial Commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from engine import prediction
+from anime_classes import animes
+from PIL import Image
+import requests
+
+def get_anime_details(anime_name):
+    url = "https://api.jikan.moe/v4/anime"
+    params = {
+        "q": anime_name,
+        "sfw": 'true'  # Correctly passing the boolean value as a string
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return f"Error: {response.status_code}, {response.json()}"
+
+
+st.title('Anime Detection App')
+
+uploaded_file = st.file_uploader("Choose an Image...", type="jpg")
+
+if uploaded_file is not None:
+    # Load and preprocess the image
+    image = Image.open(uploaded_file)
+    class_index  = prediction(uploaded_file)
+    label = animes[class_index]
+    st.write(image)
+    st.write("Prediction : ", label)
+    st.divider()
+
+    anime_details = get_anime_details(anime_name=label)
+    st.title("Anime Details")
+    st.divider()
+ 
+    if anime_details:
+        image_url = anime_details['data'][0]['images']['jpg']['image_url']
+        st.image(image_url)
+        title = anime_details['data'][0]['title']
+        st.write(f"Title : {title}")
+        st.write(f"English Title : {anime_details['data'][0]['title_english']}")
+        st.write(f"Japaneese Title : {anime_details['data'][0]['title_japanese']}")
+        # display_data(data)
+
+        genres = anime_details['data'][0]['genres']
+        st.write(f"Genres : {[name['name'] for name in genres]}")
+
+
+        st.divider()
+
+        synopsis = anime_details['data'][0]['synopsis']
+        st.write("Synopsis")
+        st.write(synopsis)
+
+        st.divider()
+
+        st.write("Trailer")
+        trailer = anime_details['data'][0]['trailer']
+        try : 
+            st.image(trailer['images']['maximum_image_url'])
+            st.write(f"Url : {trailer['url']}")
+        except:
+            st.write("Trailer not available")
+
+
+    else:
+        st.error(f"Anime '{label}' not found or API request failed.")
+    
---
+++ engine.py
@@ -0,0 +1,40 @@
+import torch
+from torchvision import models, transforms
+from PIL import Image
+import numpy as np
+from anime_classes import animes
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = models.resnet50(pretrained=True)
+
+# Modifying the fully connected layer
+num_classes = 231
+model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+model = model.to(device)
+
+# Load the model checkpoint
+model_checkpoint_path = 'Model assests/20epoch-model.pth'
+model.load_state_dict(torch.load(model_checkpoint_path, map_location=device))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize(256),  # Resize to 256x256
+    transforms.CenterCrop(224),  # Center crop to 224x224
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Function for image prediction
+def prediction(image_path):
+    # Load and preprocess the image
+    image = Image.open(image_path)
+    image = transform(image).unsqueeze(0).to(device)  # Add batch dimension and move to device
+    with torch.no_grad():
+        output = model(image)
+        output = output.cpu().numpy()
+    index = np.argmax(output)
+    return index
+
+predicted = prediction(image_path="images/goku.jpg")  # Adjust the path to your image
+label = animes[predicted]
